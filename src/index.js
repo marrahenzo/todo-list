8e2973d605ec22cba5a35143297b1283d6ab502d
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-08-05 20:45:24 -0300

    Refactored edit and create modal code

--- src/index.js
+++ src/index.js
@@ -22,11 +22,18 @@ function findProjectIndex(projects, project) {
   return -1;
 }
 
+function findTaskIndex(project, task) {
+  for (let i = 0; i < project.tasks.length; i++) {
+    if (JSON.stringify(project.tasks[i]) === JSON.stringify(task)) return i;
+  }
+  return -1;
+}
+
 let projects = [];
 
 //TODO: Remove later
 projects.push(new Project('Main'));
-let testTask = new Task('test', 'test', '1-1-1001', 'Medium');
+let testTask = new Task('test', 'test', '2022-08-05', 'Medium');
 projects[0].addTask(testTask);
 //
 
@@ -42,26 +49,63 @@ let newTaskButton = document.querySelector('#btn-new-task');
 //Modal submit button functionality
 
 let projectTitle = document.querySelector('#project-name');
+
+//Task creation
 let modalCreateContainer = document.querySelector('.modal-create-container');
-let modalSubmit = document.querySelectorAll('.modal-submit');
-let title = document.querySelector('#create-title');
-let description = document.querySelector('#create-description');
-let dueDate = document.querySelector('#create-due-date');
-let priority = document.querySelector('#create-priority');
-modalSubmit.forEach((button) => {
-  button.addEventListener('click', () => {
-    let currentProject = findProject(projects, projectTitle.textContent);
-    let newTask = new Task(
-      title.value,
-      description.value,
-      dueDate.value,
-      priority.value
-    );
-    currentProject.tasks.push(newTask);
-    addTask(taskList, currentProject, newTask);
-    closeModal(modalCreateContainer);
-  });
+let modalCreateSubmit = document.querySelectorAll('.modal-submit')[1];
+let createTitle = document.querySelector('#create-title');
+let createDescription = document.querySelector('#create-description');
+let createDueDate = document.querySelector('#create-due-date');
+let createPriority = document.querySelector('#create-priority');
+modalCreateSubmit.addEventListener('click', () => {
+  let currentProject = findProject(projects, projectTitle.textContent);
+  let newTask = new Task(
+    createTitle.value,
+    createDescription.value,
+    createDueDate.value,
+    createPriority.value
+  );
+  currentProject.tasks.push(newTask);
+  addTask(taskList, currentProject, newTask);
+  closeModal(modalCreateContainer);
+  createTitle.value = '';
+  createDescription.value = '';
+  createDueDate.value = '';
+  createPriority.value = 'Low';
 });
+
+//Task editing
+
+let modalEditSubmit = document.querySelectorAll('.modal-submit')[0];
+let editModalProject, editModalTask;
+modalEditSubmit.addEventListener('click', editTask);
+
+export function prepareEditModal(project, task) {
+  editModalProject = project;
+  editModalTask = task;
+}
+
+function editTask() {
+  let modalEditContainer = document.querySelector('.modal-edit-container');
+  let editTitle = document.querySelector('#edit-title');
+  let editDescription = document.querySelector('#edit-description');
+  let editDueDate = document.querySelector('#edit-due-date');
+  let editPriority = document.querySelector('#edit-priority');
+  let taskIndex = findTaskIndex(editModalProject, editModalTask);
+
+  let newTask = new Task(
+    editTitle.value,
+    editDescription.value,
+    editDueDate.value,
+    editPriority.value
+  );
+
+  editModalProject.tasks[taskIndex] = newTask;
+  console.log(editModalProject.tasks);
+  closeModal(modalEditContainer);
+  taskList.textContent = '';
+  loadTasks(taskList, editModalProject);
+}
 
 //If clicked, prompts for a name input, creates a new project
 // and appends it to the dom
