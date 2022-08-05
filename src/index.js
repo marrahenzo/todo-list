d5091125bb7c65efc1a341971765f1d01d5e9cf8
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-08-05 16:30:43 -0300

    Move classes to separate file

--- src/index.js
+++ src/index.js
@@ -6,56 +6,7 @@ import {
   loadTasks,
   getCurrentProject,
 } from './domScripts';
-
-class Task {
-  done = false;
-
-  constructor(name, description, dueDate, priority) {
-    this.name = name;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-  }
-
-  toggleDone() {
-    this.done = !this.done;
-  }
-}
-
-class Project {
-  tasks = [];
-
-  constructor(name) {
-    this.name = name;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    if (typeof newName === 'string') this._name = newName;
-  }
-
-  addTask(task) {
-    this.tasks.push(task);
-  }
-
-  removeTask(task) {
-    tasks[this.findTask(task)];
-  }
-
-  findTask(task) {
-    let index = this.tasks.indexOf(task);
-    return index;
-  }
-}
-
-const Priority = Object.freeze({
-  High: 'High',
-  Medium: 'Medium',
-  Low: 'Low',
-});
+import { Task, Project, Priority } from './classes';
 
 let projects = [];
 
