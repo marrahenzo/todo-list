e233c1bdf64d036a195834071f045e8365063966
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-08-05 15:32:21 -0300

    Mov classes to separate file

---
+++ src/classes.js
@@ -0,0 +1,51 @@
+class Task {
+  done = false;
+
+  constructor(name, description, dueDate, priority) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  toggleDone() {
+    this.done = !this.done;
+  }
+}
+
+class Project {
+  tasks = [];
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') this._name = newName;
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+
+  removeTask(task) {
+    tasks[this.findTask(task)];
+  }
+
+  findTask(task) {
+    let index = this.tasks.indexOf(task);
+    return index;
+  }
+}
+
+const Priority = Object.freeze({
+  High: 'High',
+  Medium: 'Medium',
+  Low: 'Low',
+});
+
+export { Task, Project, Priority };
